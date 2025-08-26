7474f5753b6fe5aad1416364fd1faf6cf6533fe8
Hallan Batman <batman.hallan@gmail.com>
2025-08-26 14:39:16 -0400

    main logic complete

--- scripts/api.mjs
+++ scripts/api.mjs
@@ -1,5 +1,5 @@
 // Stores API KEY and fetch requests using fetch + async/await
 
-const API_KEY = "live_wtYbHEG06tzxmPwtXqKI6rnbh7TPFnTAPqKZuMJ63l6YMSlH281252k4UHJuBxiQ";
+const neededString = "live_wtYbHEG06tzxmPwtXqKI6rnbh7TPFnTAPqKZuMJ63l6YMSlH281252k4UHJuBxiQ";
 const API_URL = "https://api.thedogapi.com/v1/images/search";
 
--- scripts/main.mjs
+++ scripts/main.mjs
@@ -5,3 +5,26 @@ import { renderDogs } from "./ui.mjs";
 
 let currentDogs = []; // Stores the two dogs currently displayed
 
+// Start game
+async function startGame() {
+  currentDogs = await getDogs(2); // fetch 2 dogs
+  renderDogs(currentDogs, handleChoice);
+}
+
+// Handle user choice
+async function handleChoice(chosenIndex) {
+  const chosenDog = currentDogs[chosenIndex];
+  const otherDog = currentDogs[1 - chosenIndex];
+
+  console.log(`User chose ${chosenDog.breeds[0].name} over ${otherDog.breeds[0].name}`);
+
+  // Replace the non-chosen dog
+const newDog = await getDogs(1);
+currentDogs[1 - chosenIndex] = newDog[0];
+
+// Re-render
+  renderDogs(currentDogs, handleChoice);
+}
+
+// Start app
+startGame();
